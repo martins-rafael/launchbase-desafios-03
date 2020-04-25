38c2cb5ff4c5fcfe03959feb65a81ef38d2c8d80
Rafael Martins <rafaeldcmartins@gmail.com>
2020-04-24 22:42:07 -0300

    added hide button

--- 03-4-refatorando-foodfy/public/js/script.js
+++ 03-4-refatorando-foodfy/public/js/script.js
@@ -1,8 +1,22 @@
 const recipes = document.querySelectorAll('.recipe')
+const recipeWrapers = document.querySelectorAll('.recipe-wraper')
 
 for (let recipe of recipes) {
-    recipe.addEventListener('click', function() {
+    recipe.addEventListener('click', function () {
         const recipeIndex = recipe.getAttribute('id')
         window.location.href = `/recipes/${recipeIndex}`
+    })
+}
+
+for (let wraper of recipeWrapers) {
+    const hide = wraper.querySelector('.hide')
+
+    hide.addEventListener('click', function () {
+        wraper.querySelector('.wraper-content').classList.toggle('hidden')
+        if (hide.innerHTML == 'ESCONDER') {
+            hide.innerHTML = 'MOSTRAR'
+        } else {
+            hide.innerHTML = 'ESCONDER'
+        }
     })
 }
